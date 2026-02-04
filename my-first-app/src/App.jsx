d92e7a940916c79e87a91afbe8f9bef42b344a38
Rejaul Karim <iamcrk.dev@gmail.com>
2026-02-04 17:28:09 +0600

    Add login component

--- my-first-app/src/App.jsx
+++ my-first-app/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './components/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h2>Hello World</h2>
+      <Login />
     </>
   )
 }
